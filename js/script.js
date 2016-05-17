9d5efd9a4fd05ca7c472f53a559bf9626840dceb
Joni Halabi <jh1955@georgetown.edu>
2016-05-17 15:44:29 -0400

    Feedback on homework #9

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+  /* Fantastic!  Love the selector - and your use of the popovers */
  $('[data-toggle="popover"]').popover(); 
+
+ /* Is there a carousel on the site?  I did not see one. */
  $('.carousel').carousel()
 });
